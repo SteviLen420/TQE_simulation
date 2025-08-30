cf7b8543e279b9eb34239d32fb9cafb7cd8c8b66
SteviLen420 <lengyelstevi81@gmail.com>
2025-08-30 14:36:52 +0200

    Update 07_TQE_(E)_UniverseSimulation_LawLockin.py

--- 07_TQE_(E)_UniverseSimulation_LawLockin/07_TQE_(E)_UniverseSimulation_LawLockin.py
+++ 07_TQE_(E)_UniverseSimulation_LawLockin/07_TQE_(E)_UniverseSimulation_LawLockin.py
@@ -61,7 +61,7 @@ PLOT_LOCKIN_HIST = True     # histogram of lock-in epochs
 # Master hyperparameters
 EXPANSION_EPOCHS = 1000      # epochs for expansion dynamics (t > 0)
 LOCKIN_EPOCHS    = 500      # epochs for law lock-in simulation
-NUM_UNIVERSES    = 1000     # set to 1 for single-universe mode
+NUM_UNIVERSES    = 2000     # set to 1 for single-universe mode
 
 # ======================================================
 # 1) t < 0 : Quantum superposition (vacuum fluctuation)
