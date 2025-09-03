34f63ed3f628f0955eaf4849b3a58d35122c0e33
SteviLen420 <lengyelstevi81@gmail.com>
2025-09-03 21:32:44 +0200

    Update 02_TQE_(E,I)_UNIVERSE_SIMULATION_io_paths.py

--- 01_TQE_(E,I)_UNIVERSE_SIMULATION/01_TQE_(E,I)_UNIVERSE_SIMULATION/02_TQE_(E,I)_UNIVERSE_SIMULATION_io_paths.py
+++ 01_TQE_(E,I)_UNIVERSE_SIMULATION/01_TQE_(E,I)_UNIVERSE_SIMULATION/02_TQE_(E,I)_UNIVERSE_SIMULATION_io_paths.py
@@ -9,9 +9,6 @@
 # Author: Stefan Len
 # ===================================================================================
 
-from config import ACTIVE        # load the active master controller settings
-from imports import *            # common imports (numpy, matplotlib, qutip, etc.)
-
 import os, platform, time, pathlib
 from typing import Dict, List
 
