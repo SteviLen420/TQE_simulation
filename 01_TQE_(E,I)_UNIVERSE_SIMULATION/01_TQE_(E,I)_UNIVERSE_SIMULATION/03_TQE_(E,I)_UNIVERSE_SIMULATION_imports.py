f03dbc5a349ee6b9c3848777d231c1ecfdad44a5
SteviLen420 <lengyelstevi81@gmail.com>
2025-09-03 21:32:37 +0200

    Update 03_TQE_(E,I)_UNIVERSE_SIMULATION_imports.py

--- 01_TQE_(E,I)_UNIVERSE_SIMULATION/01_TQE_(E,I)_UNIVERSE_SIMULATION/03_TQE_(E,I)_UNIVERSE_SIMULATION_imports.py
+++ 01_TQE_(E,I)_UNIVERSE_SIMULATION/01_TQE_(E,I)_UNIVERSE_SIMULATION/03_TQE_(E,I)_UNIVERSE_SIMULATION_imports.py
@@ -6,9 +6,6 @@
 # Author: Stefan Len
 # ===================================================================================
 
-from config import ACTIVE        # load the active master controller settings
-from imports import *            # common imports (numpy, matplotlib, qutip, etc.)
-
 # ---------------------------
 # Standard Python libraries
 # ---------------------------
