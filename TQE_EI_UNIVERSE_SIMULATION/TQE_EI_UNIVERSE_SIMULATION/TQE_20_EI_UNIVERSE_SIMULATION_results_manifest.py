50b8925a2bc50d1718d0068504ee4c43ff54b06f
SteviLen420 <lengyelstevi81@gmail.com>
2025-09-06 15:10:32 +0200

    Update TQE_20_EI_UNIVERSE_SIMULATION_results_manifest.py

--- TQE_EI_UNIVERSE_SIMULATION/TQE_EI_UNIVERSE_SIMULATION/TQE_20_EI_UNIVERSE_SIMULATION_results_manifest.py
+++ TQE_EI_UNIVERSE_SIMULATION/TQE_EI_UNIVERSE_SIMULATION/TQE_20_EI_UNIVERSE_SIMULATION_results_manifest.py
@@ -10,8 +10,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from config import ACTIVE                      # unified master config
-from io_paths import resolve_output_paths, ensure_colab_drive_mounted
+from TQE_03_EI_UNIVERSE_SIMULATION_imports import ACTIVE, PATHS, RUN_DIR, FIG_DIR
 
 # ---------------------------
 # Utilities
