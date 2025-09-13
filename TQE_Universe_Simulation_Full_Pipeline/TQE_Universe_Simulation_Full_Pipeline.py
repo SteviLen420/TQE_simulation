69148e5c09a5236ba2e3ed2587b2217f21eaddbd
Stefan Len <228879170+SteviLen420@users.noreply.github.com>
2025-09-13 21:04:27 +0200

    Update TQE_Universe_Simulation_Full_Pipeline.py

--- TQE_Universe_Simulation_Full_Pipeline/TQE_Universe_Simulation_Full_Pipeline.py
+++ TQE_Universe_Simulation_Full_Pipeline/TQE_Universe_Simulation_Full_Pipeline.py
@@ -26,6 +26,7 @@ if IN_COLAB:
 import os, sys, subprocess, importlib
 
 PKGS = {
+    "cython":        "<3.0", 
     "numpy":         "1.26.4",
     "numba":         "0.59.1",
     "scipy":         "1.13.1",
