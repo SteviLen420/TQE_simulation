748744a5b030bf02a8af4275f7f6c413191970b1
Stefan Len <228879170+SteviLen420@users.noreply.github.com>
2025-09-15 12:29:18 +0200

    Update TQE_Universe_Simulation_Full_Pipeline.py

--- TQE_Universe_Simulation_Full_Pipeline/TQE_Universe_Simulation_Full_Pipeline.py
+++ TQE_Universe_Simulation_Full_Pipeline/TQE_Universe_Simulation_Full_Pipeline.py
@@ -58,8 +58,8 @@ except Exception:
 # ======================================================
 MASTER_CTRL = {
     # --- Core simulation ---
-    "NUM_UNIVERSES":        10000,   # number of universes in Monte Carlo run
-    "TIME_STEPS":           2500,    # epochs per stability run (if used elsewhere)
+    "NUM_UNIVERSES":        7500,   # number of universes in Monte Carlo run
+    "TIME_STEPS":           2000,    # epochs per stability run (if used elsewhere)
     "LOCKIN_EPOCHS":        1000,    # epochs for law lock-in dynamics
     "EXPANSION_EPOCHS":     1500,    # epochs for expansion dynamics
     "FL_EXP_EPOCHS":        800,    # length of t>0 expansion panel
